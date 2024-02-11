d637c499146c71dda468d497599d0b4295c87876
nickberes <nicolae.beres@cumulocity.com>
2024-02-11 16:27:35 +0200

    added coin but collision does not work

--- Game/Script/Collectable_Coin.gd
+++ Game/Script/Collectable_Coin.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 @onready var visual : Node3D = $VisualNode
 @onready var pickpupVFX: GPUParticles3D = $VFXNode
-@onready var animationPlayer: AnimationPlayer = $AnimationPlayer
+#@onready var animationPlayer: AnimationPlayer = $AnimationPlayer
 
 var rotateSpeed = 1
 var coinValue = 1
@@ -11,13 +11,15 @@ var coinValue = 1
 func _process(delta):
 	visual.rotate_y(rotateSpeed * delta)
 
-	if visual.visible == false && pickpupVFX.emitting == false:
-		queue_free()
+	#if visual.visible == false && pickpupVFX.emitting == false:
+		#queue_free()
 
 func _on_area_3d_body_entered(body):
-	if body.is_in_group("Player"):
-		pickpupVFX.emitting = true
-		animationPlayer.play("Collected")
+	if body.is_in_group($"../Player"):
+		queue_free()
+		#pickpupVFX.emitting = true
+		#visual.visible = false
+		#animationPlayer.play("Collected")
 		
-		body.AddCoin(coinValue)
+		#body.AddCoin(coinValue)
 		
