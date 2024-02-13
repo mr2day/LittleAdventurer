a8ea2a5ae451548fe14137c46cf237f048604e46
nick <beres.nick@gmail.com>
2024-02-13 02:06:39 +0200

    added enemy simple navigation script

---
+++ Game/Script/Enemy_AI.gd
@@ -0,0 +1,34 @@
+extends CharacterBody3D
+
+const SPEED = 0.5
+
+var player: Node3D
+
+@onready var navAgent: NavigationAgent3D = $NavigationAgent3D
+@onready var visual: Node3D = $VisualNode
+@onready var animationPlayer: AnimationPlayer = $VisualNode/AnimationPlayer
+
+var direction: Vector3
+var stopDistance: float = 2.2
+
+func _ready():
+	player = get_tree().get_first_node_in_group("Player")
+
+func _physics_process(delta):
+	navAgent.target_position = player.global_position
+	
+	direction = navAgent.get_next_path_position() - global_position
+	direction.normalized()
+	
+	if navAgent.distance_to_target() < stopDistance:
+		animationPlayer.play("NPC_01_IDEL")
+		return
+	
+	velocity = velocity.lerp(direction * SPEED, delta)
+	animationPlayer.play("NPC_01_WALK")
+	
+	if velocity.length() > 0.2:
+		var lookDir = Vector2(velocity.z, velocity.x)
+		visual.rotation.y = lookDir.angle()
+	
+	move_and_slide()
